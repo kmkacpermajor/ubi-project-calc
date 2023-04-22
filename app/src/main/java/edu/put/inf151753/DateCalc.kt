937ce65dde08977a82860836e0d48f5da5cd50ae
kmkacpermajor <kmkacpermajor@gmail.com>
2023-04-22 15:38:50 +0200

    Working app

--- app/src/main/java/edu/put/inf151753/DateCalc.kt
+++ app/src/main/java/edu/put/inf151753/DateCalc.kt
@@ -1,14 +1,144 @@
 package edu.put.inf151753
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.Month
+import java.time.temporal.ChronoUnit
+
 
 class DateCalc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date_calc)
+
+        val topDatePicker = findViewById<DatePicker>(R.id.topDatePicker)
+        val bottomDatePicker = findViewById<DatePicker>(R.id.bottomDatePicker)
+
+        topDatePicker.setOnDateChangedListener{
+            _, _, _, _ -> dateChangedHandler()
+        }
+
+        bottomDatePicker.setOnDateChangedListener{
+                _, _, _, _ -> dateChangedHandler()
+        }
+    }
+
+    fun dateChangedHandler(){
+        calculateDayDifference()
+        calculateWorkingDayDifference()
+    }
+
+    fun calculateDayDifference(){
+        val topDatePicker = findViewById<DatePicker>(R.id.topDatePicker)
+        val bottomDatePicker = findViewById<DatePicker>(R.id.bottomDatePicker)
+
+        val daysBetweenView = findViewById<EditText>(R.id.daysBetweenValue)
+
+        val topDate = LocalDate.of(topDatePicker.year, topDatePicker.month, topDatePicker.dayOfMonth)
+        val bottomDate = LocalDate.of(bottomDatePicker.year, bottomDatePicker.month, bottomDatePicker.dayOfMonth)
+
+        daysBetweenView.setText(ChronoUnit.DAYS.between(topDate, bottomDate).toString())
+    }
+
+    fun calculateEaster(year: Int): LocalDate{
+        if (year == 2018){
+            return LocalDate.of(2018, Month.NOVEMBER, 12)
+        }
+
+        if(year == 2005){
+            return LocalDate.of(2005, Month.APRIL, 8)
+        }
+
+        val a = year % 19
+        val b = year / 100
+        val c = year % 100
+        val d = b / 4
+        val e = b % 4
+        val f = (b + 8) / 25
+        val g = (b - f + 1) / 3
+        val h = (19 * a + b - d - g + 15) % 30
+        val i = c / 4
+        val k = c % 4
+        val l = (32 + 2 * e + 2 * i - h - k) % 7
+        val m = (a + 11 * h + 22 * l) / 451
+        val p = (h + l - 7 * m + 114) % 31
+
+        val day = p + 1
+        val month = (h + l - 7 * m + 114) / 31
+
+        return LocalDate.of(year, month, day)
+    }
+
+    fun getHolidaysBetween(first: LocalDate, second: LocalDate): List<LocalDate>{
+        val years = (first.year.. second.year).toList()
+
+        val holidays = mutableListOf<LocalDate>()
+
+        for (year in years){
+            holidays.add(LocalDate.of(year, Month.JANUARY, 1))
+            holidays.add(LocalDate.of(year, Month.JANUARY, 6))
+            holidays.add(LocalDate.of(year, Month.MAY, 1))
+            holidays.add(LocalDate.of(year, Month.MAY, 3))
+            holidays.add(LocalDate.of(year, Month.AUGUST, 15))
+            holidays.add(LocalDate.of(year, Month.NOVEMBER, 1))
+            holidays.add(LocalDate.of(year, Month.NOVEMBER, 11))
+            holidays.add(LocalDate.of(year, Month.DECEMBER, 25))
+            holidays.add(LocalDate.of(year, Month.DECEMBER, 26))
+
+            val easter = calculateEaster(year)
+            holidays.add(easter)
+            holidays.add(easter.plusDays(1))
+            holidays.add(easter.plusDays(60))
+        }
+
+        return holidays.toList()
+    }
+    fun getWorkingDaysBetween(first: LocalDate, second: LocalDate): Int{
+        val workingDates = mutableListOf<LocalDate>()
+        val holidays = getHolidaysBetween(first, second)
+        var current = first
+        while(current != second){
+            if (current.dayOfWeek != DayOfWeek.SATURDAY
+                && current.dayOfWeek != DayOfWeek.SUNDAY
+                && !holidays.contains(current)) {
+
+                workingDates.add(current)
+            }
+            current = current.plusDays(1)
+        }
+
+        return workingDates.count()
+    }
+
+    fun calculateWorkingDayDifference() {
+        val topDatePicker = findViewById<DatePicker>(R.id.topDatePicker)
+        val bottomDatePicker = findViewById<DatePicker>(R.id.bottomDatePicker)
+
+        val workingDaysBetweenView = findViewById<TextView>(R.id.workingDaysBetweenValue)
+
+        val topDate =
+            LocalDate.of(topDatePicker.year, topDatePicker.month+1, topDatePicker.dayOfMonth)
+        val bottomDate =
+            LocalDate.of(bottomDatePicker.year, bottomDatePicker.month+1, bottomDatePicker.dayOfMonth)
+
+        workingDaysBetweenView.text = getWorkingDaysBetween(topDate, bottomDate).toString()
+    }
+
+
+    fun addDaysToBottom(view: View){
+        val bottomDatePicker = findViewById<DatePicker>(R.id.bottomDatePicker)
+        val daysBetweenView = findViewById<EditText>(R.id.daysBetweenValue)
+
+        var date = LocalDate.of(bottomDatePicker.year, bottomDatePicker.month, bottomDatePicker.dayOfMonth)
+        date = date.plusDays(daysBetweenView.text.toString().toLong())
+
+        bottomDatePicker.updateDate(date.year, date.monthValue, date.dayOfMonth)
     }
 
     fun goBack(view: View){
--- app/src/main/java/edu/put/inf151753/TimeCalc.kt
+++ app/src/main/java/edu/put/inf151753/TimeCalc.kt
@@ -1,11 +1,9 @@
 package edu.put.inf151753
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 
 class TimeCalc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +11,96 @@ class TimeCalc : AppCompatActivity() {
         setContentView(R.layout.activity_time_calc)
     }
 
-    fun goBack(view: View){
+    fun goBack(view: View) {
         this.finish()
     }
 
-    fun clearBottom(view: View){
+    fun addTime(view: View) {
+        val hourTopView = findViewById<EditText>(R.id.hourTop)
+        val minuteTopView = findViewById<EditText>(R.id.minuteTop)
+        val secondTopView = findViewById<EditText>(R.id.secondTop)
+
+        val hourBottomView = findViewById<EditText>(R.id.hourBottom)
+        val minuteBottomView = findViewById<EditText>(R.id.minuteBottom)
+        val secondBottomView = findViewById<EditText>(R.id.secondBottom)
+
+        var hourTop: Int = hourTopView.text.toString().toInt()
+        var minuteTop: Int = minuteTopView.text.toString().toInt()
+        var secondTop: Int = secondTopView.text.toString().toInt()
+
+        val hourBottom: Int = hourBottomView.text.toString().toInt()
+        val minuteBottom: Int = minuteBottomView.text.toString().toInt()
+        val secondBottom: Int = secondBottomView.text.toString().toInt()
+
+        secondTop += secondBottom
+        while (secondTop >= 60){
+            minuteTop++
+            secondTop -= 60
+        }
+
+        minuteTop += minuteBottom
+        while (minuteTop >= 60){
+            hourTop++
+            minuteTop -= 60
+        }
+
+        hourTop += hourBottom
+
+        hourTopView.setText(hourTop.toString())
+        minuteTopView.setText(minuteTop.toString())
+        secondTopView.setText(secondTop.toString())
+        clearBottom(view)
+    }
+
+    fun subtractTime(view: View) {
+        val hourTopView = findViewById<EditText>(R.id.hourTop)
+        val minuteTopView = findViewById<EditText>(R.id.minuteTop)
+        val secondTopView = findViewById<EditText>(R.id.secondTop)
+
+        val hourBottomView = findViewById<EditText>(R.id.hourBottom)
+        val minuteBottomView = findViewById<EditText>(R.id.minuteBottom)
+        val secondBottomView = findViewById<EditText>(R.id.secondBottom)
+
+        var hourTop: Int = hourTopView.text.toString().toInt()
+        var minuteTop: Int = minuteTopView.text.toString().toInt()
+        var secondTop: Int = secondTopView.text.toString().toInt()
+
+        val hourBottom: Int = hourBottomView.text.toString().toInt()
+        val minuteBottom: Int = minuteBottomView.text.toString().toInt()
+        val secondBottom: Int = secondBottomView.text.toString().toInt()
+
+        secondTop -= secondBottom
+        while (secondTop < 0){
+            --minuteTop
+            secondTop += 60
+        }
+
+
+        minuteTop -= minuteBottom
+        while (minuteTop < 0){
+            --hourTop
+            minuteTop += 60
+        }
+
+        hourTop -= hourBottom
+
+        hourTopView.setText(hourTop.toString())
+        minuteTopView.setText(minuteTop.toString())
+        secondTopView.setText(secondTop.toString())
+        clearBottom(view)
+    }
+
+    fun clearTop(view: View) {
+        val hour = findViewById<EditText>(R.id.hourTop)
+        val minute = findViewById<EditText>(R.id.minuteTop)
+        val second = findViewById<EditText>(R.id.secondTop)
+
+        hour.setText("0")
+        minute.setText("0")
+        second.setText("0")
+    }
+
+    fun clearBottom(view: View) {
         val hour = findViewById<EditText>(R.id.hourBottom)
         val minute = findViewById<EditText>(R.id.minuteBottom)
         val second = findViewById<EditText>(R.id.secondBottom)
@@ -25,5 +108,10 @@ class TimeCalc : AppCompatActivity() {
         hour.setText("0")
         minute.setText("0")
         second.setText("0")
+    }
+
+    fun clearBoth(view: View) {
+        clearTop(view)
+        clearBottom(view)
     }
 }
